3c8c5b79eb7d07cd1e83d8583d528e26483868f0
Msoteras <msoteras@frba.utn.edu.ar>
2022-04-22 09:39:45 -0300

    Continuo trabajando con lo visto en clase

--- src/Library.hs
+++ src/Library.hs
@@ -5,33 +5,52 @@ doble :: Number -> Number
 doble numero = numero + numero
 
 
-data Hechicero = Hechicero {
-        nombre :: String,
+data Hechicero = UnHechicero {
+        --nombre :: String, no se pide luego en los ejercicos el mostrar el nombre
         antiguedad :: Number,
-        experiencia :: String,
+       -- experiencia :: String,
         clan :: String,
         grado :: Number
 }deriving (Show)
 
 -- Hechiceros
-nobara = Hechicero {nombre = "Nobara", antiguedad = 1, experiencia = "Estudiante", clan = "Kugisaki", grado = 3}
-satoru = Hechicero {nombre = "Satoru", antiguedad = 15, experiencia = "Maestro", clan = "Gajo", grado = 0}
-maki = Hechicero {nombre = "Maki", antiguedad = 3, experiencia = "Estudiante", clan = "Zenin", grado = 4}
-yuji = Hechicero {nombre = "Yuji", antiguedad = 0, experiencia = "Novato", clan = "Kugisaki", grado = 1}
+nobara = UnHechicero {antiguedad = 1, clan = "Kugisaki", grado = 3}
+satoru = UnHechicero {antiguedad = 15, clan = "Gajo", grado = 0}
+maki = UnHechicero {antiguedad = 3, clan = "Zenin", grado = 4}
+yuji = UnHechicero {antiguedad = 0, clan = "Kugisaki", grado = 1}
 
 
-tieneExperiencia :: Hechicero -> String --o con bool
-tieneExperiencia hechicero | grado hechicero >= 1 = "Tiene experiencia"
-                           | otherwise = "No tiene experiencia"
+tieneExperiencia :: Hechicero -> Bool
+tieneExperiencia hechicero = grado hechicero > 1
 
 type Equipo = [Hechicero]
 equipoPreparado :: Equipo -> Bool
-equipoPreparado equipo = length equipo >= 3
+equipoPreparado equipo = length equipo > 3
 
 subirGrado :: Hechicero -> Hechicero
-subirGrado hechicero | grado hechicero == 0 = hechicero
-                     | otherwise = Hechicero {nombre = nombre hechicero, antiguedad = antiguedad hechicero, experiencia = experiencia hechicero, clan = clan hechicero, grado = grado hechicero - 1} 
+subirGrado hechicero | esEspecial hechicero = hechicero
+                     | otherwise = hechicero {grado = grado hechicero - 1} 
+
+esEspecial :: Hechicero -> Bool
+esEspecial hechicero = grado hechicero == 0
+
 
 clanesPrestigiosos = ["Zenin", "Gojo", "Kamo"]
 esPrestigioso :: Hechicero -> Bool
 esPrestigioso hechicero = elem (clan hechicero) clanesPrestigiosos 
+
+-- Punto 6
+esInvencible :: Equipo -> Bool
+esInvencible equipo = any esEspecial equipo
+
+--Punto 7
+esFavorito :: Equipo -> Bool
+esFavorito equipo = all esPrestigioso equipo
+
+--Punto 8
+losExpertos :: Equipo -> Equipo
+losExpertos equipo = filter tieneExperiencia equipo
+
+--Punto 9
+powerUp :: Equipo -> Equipo
+powerUp equipo = map subirGrado equipo
